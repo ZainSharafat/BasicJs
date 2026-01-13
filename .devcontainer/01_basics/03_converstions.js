9e592cc479ebfa0d25a6a5ceaedd3588ad589aa8
Muhammad Zain <147588685+ZainSharafat@users.noreply.github.com>
2026-01-13 11:46:52 +0000

    converstion of datatypes

---
+++ .devcontainer/01_basics/03_converstions.js
@@ -0,0 +1,29 @@
+let score = true;
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+// "33" = > 33
+// "33abc" => NaN
+// true => 1; False => 0
+
+let isLoggedIn = 1
+
+let changeInBoolean = Boolean(isLoggedIn)
+
+console.log(changeInBoolean);
+
+
+let someNumber = 123;
+
+let changeInString = String(someNumber);
+
+console.log(changeInString);
+
+console.log(typeof (changeInString));
+
