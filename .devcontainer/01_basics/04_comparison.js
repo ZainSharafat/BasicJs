994b307a7189ce2195ca38a4a6cac0284b43ce2c
Muhammad Zain <147588685+ZainSharafat@users.noreply.github.com>
2026-01-13 12:27:22 +0000

    comparison and equility

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+console.log(2>1);
+console.log(2 >= 1);
+console.log(1 != 1);
+// answer in boolean
+
+console.log("2" > 1); // in converstion --> same datatype (In TypeScript)
+
+console.log("02" > 1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined == 0)
+console.log(undefined < 0)
+console.log(undefined > 0)
+
+// comparison and equality is different in javascript
+
+//===    --> check value strickly
+
+console.log("1"===1);
+
+//clean code is most important
