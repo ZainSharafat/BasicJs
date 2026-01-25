dd4f5411154c6b8e02257416aa8804949057306e
Muhammad Zain <147588685+ZainSharafat@users.noreply.github.com>
2026-01-25 22:24:59 +0000

    arraybasic

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+const myArray = [0, 1, 2, 3, 4, 5]
+
+//console.log(myArray[2]);
+
+//const myHeros = ["Muhammad", "sharafat"]
+
+//another way to declare an array
+//const newArray = new Array(1,2,3,4)
+////console.log(newArray[0]);
+
+//Array Methods
+
+//myArray.push(6)
+//myArray.push(7)
+//myArray.pop() //remove last element
+
+//myArray.unshift(9)
+//myArray.shift() //first digit will remove
+
+
+//const newArray1 = myArray.join()
+
+//console.log(newArray1);
+//console.log(myArray);
+//console.log(typeof (newArray1))
+
+
+
+//slice and splice
+
+console.log("A ",myArray);
+//slice
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B ", myArray)
+
+//splice
+const myn2 = myArray.splice(1,3)
+console.log("C ", myArray)
+console.log(myn2);
+
+
