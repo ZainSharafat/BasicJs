073a988e6c7ad76e06b5a8b772a0b3b5a386772d
Muhammad Zain <147588685+ZainSharafat@users.noreply.github.com>
2026-01-26 02:21:02 +0000

    JSON Format basic intro

---
+++ .devcontainer/03_objects/03objects.js
@@ -0,0 +1,30 @@
+const course = {
+    coursename : "Jswithzain",
+    price : 2999,
+    courseinstructor : "Zain",
+}
+
+
+//2 methods to access
+//1) course.courseinstructor
+
+//2) //destructuring
+const {courseinstructor: Instructor} = course
+//console.log(courseinstructor);
+console.log(Instructor);
+
+{
+    //JSON
+}
+
+{
+    name : "zain"
+    price : "5999"
+    course : "JS"
+}
+
+[
+    {},
+    {},
+
+]
